120c1385a9b094206b58a19d8ff58006b9981ea2
C0kyez <felipemauricio.mario@gmail.com>
2024-09-24 17:57:18 -0300

    Entrega avance 1
    Arregle las dependencias

---
+++ mail_box_app/README.md
@@ -0,0 +1,3 @@
+# mail_box_app
+
+A new Flutter project.
---
+++ mail_box_app/android/app/src/main/kotlin/com/example/mail_box_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mail_box_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
